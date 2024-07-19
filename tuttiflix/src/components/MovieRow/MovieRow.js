ff22f84bf1b1d7094e5d5e3119f64b0614b2f0bd
Luis Eduardo <luiseduardf.a@gmail.com>
2024-07-19 16:30:36 -0300

    sytles
    style moviRow

--- tuttiflix/src/components/MovieRow/MovieRow.js
+++ tuttiflix/src/components/MovieRow/MovieRow.js
@@ -5,12 +5,12 @@ import './styles.css';
 
 export default ({ title, items }) => {
     return (
-        <div>
+        <div className="movieRow">
             <h2>{title}</h2>
             <div className="movieRow-listarea">
                 <div className="movieRow--list">
                     {items.results.length > 0 && items.results.map((item, key) => (
-                        <div key={key} className="moviRow-item">
+                        <div key={key} className="movieRow-item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
                         </div>
                     ))}
